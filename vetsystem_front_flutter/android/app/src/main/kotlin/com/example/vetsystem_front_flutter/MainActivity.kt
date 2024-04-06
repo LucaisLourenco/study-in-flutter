a454c45b0496fdfb7a2accab1b47ed33e5927221
lucas.lourenco <lucaspeixe2018@gmail.com>
2024-04-06 16:01:43 -0300

    Iniciando projeto flutter

--- vetsystem_front_flutter/android/app/src/main/kotlin/com/example/vetsystem_front_flutter/MainActivity.kt
+++ vetsystem_front_flutter/android/app/src/main/kotlin/com/example/vetsystem_front_flutter/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.vetsystem_front_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
+//package com.example.vetsystem_front_flutter
+//
+//import io.flutter.embedding.android.FlutterActivity
+//
+//class MainActivity: FlutterActivity()
