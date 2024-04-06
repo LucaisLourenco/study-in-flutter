bf3cad836b4b549370945aedb448b5e0016b599d
lucas.lourenco <lucaspeixe2018@gmail.com>
2024-04-06 16:18:09 -0300

    Iniciando projeto flutter

---
+++ vetsystem_front_flutter/lib/views/login_screen.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class AuthService {
+  static const String _baseUrl = 'https://yourapi.com/api';
+  static const String _loginEndpoint = '/login';
+
+  static Future<bool> login(String username, String password) async {
+    final response = await http.post(
+      Uri.parse(_baseUrl + _loginEndpoint),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'email': username,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      // Aqui você salvaria o token JWT recebido em algum lugar seguro (SharedPreferences, por exemplo)
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
